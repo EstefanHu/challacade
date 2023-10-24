b6503ff30f2614dacf018760b23511bda5305976
Estefan <estefanhu074@gmail.com>
2023-10-23 17:42:19 -0700

    added walking animation

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 function love.load()
     anim8 = require "lib/anim8"
+    love.graphics.setDefaultFilter("nearest", "nearest")
 
     player = {}
     player.x = 400
@@ -9,34 +10,51 @@ function love.load()
     player.grid = anim8.newGrid(12, 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
 
     player.animations = {}
-    player.animations.down = anim8.newAnimation(player.grid('1-4', '1'), 0.2)
+    player.animations.down = anim8.newAnimation(player.grid('1-4', 1), 0.2)
+    player.animations.left = anim8.newAnimation(player.grid('1-4', 2), 0.2)
+    player.animations.right = anim8.newAnimation(player.grid('1-4', 3), 0.2)
+    player.animations.up = anim8.newAnimation(player.grid('1-4', 4), 0.2)
+
+    player.anim = player.animations.left
 
     background = love.graphics.newImage("sprites/background.png")
 end
 
 function love.update(dt)
+    local isMoving = false
     
     if love.keyboard.isDown("right") then
+        isMoving = true
+        player.anim = player.animations.right
         player.x = player.x + player.speed
     end
     
     if love.keyboard.isDown("left") then
+        isMoving = true
+        player.anim = player.animations.left
         player.x = player.x - player.speed 
     end
     
     if love.keyboard.isDown("down") then
+        isMoving = true
+        player.anim = player.animations.down
         player.y = player.y + player.speed
     end
     
     if love.keyboard.isDown("up") then
+        isMoving = true
+        player.anim = player.animations.up
         player.y = player.y - player.speed 
     end
 
-    player.animations.down:update(dt)
+    if isMoving == false then
+        player.anim:gotoFrame(2)
+    end
 
+    player.anim:update(dt)
 end
 
 function love.draw()
     love.graphics.draw(background, 0, 0)
-    player.animations.down:draw(player.spriteSheet, player.x, player.y)
+    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 10)
 end
