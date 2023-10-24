c3c3e43c085a5cf8d04a167b1f54ae4fcf42c1b6
Estefan <estefanhu074@gmail.com>
2023-10-23 23:00:12 -0700

    added map

--- main.lua
+++ main.lua
@@ -2,6 +2,9 @@ function love.load()
     anim8 = require "lib/anim8"
     love.graphics.setDefaultFilter("nearest", "nearest")
 
+    sti = require 'lib/sti'
+    gameMap = sti('maps/testMap.lua')
+
     player = {}
     player.x = 400
     player.y = 200
@@ -55,6 +58,6 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.draw(background, 0, 0)
-    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 10)
+    gameMap:draw()
+    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 6)
 end
